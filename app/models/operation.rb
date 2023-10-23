da9e518a2df38153fee12d6c984d08f83ee8ea61
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-10-23 18:08:07 -0300

    Add relations and validations to operations model

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,3 +1,8 @@
 class Operation < ApplicationRecord
-  belongs_to :author
+  belongs_to :author, , class_name: 'User', foreign_key: 'author_id'
+  belongs_to :group, optional: true
+
+  validates :name, presence: true
+  validates :amount, presence: true
+  validates :created_at, presence: true
 end
