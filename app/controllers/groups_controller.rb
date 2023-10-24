d5b5798887c51038f4719f65012b33690b36a10c
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-10-24 00:23:38 -0300

    Update groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,26 @@
 class GroupsController < ApplicationController
-  def index; end
+  def index
+    @groups = Group.all
+  end
 
   def show; end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to groups_path, notice: 'Group was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
 end
