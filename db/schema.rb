58bd13cd8ba71f3de76d8e184e4d6bb0bedd4931
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-10-24 19:03:50 -0300

    Update migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_23_203034) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_24_154330) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_23_203034) do
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "operations", force: :cascade do |t|
@@ -27,7 +29,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_23_203034) do
     t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "group_id", null: false
     t.index ["author_id"], name: "index_operations_on_author_id"
+    t.index ["group_id"], name: "index_operations_on_group_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,5 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_23_203034) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "groups", "users"
+  add_foreign_key "operations", "groups"
   add_foreign_key "operations", "users", column: "author_id"
 end
