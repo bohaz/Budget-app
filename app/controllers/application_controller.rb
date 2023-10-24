3d6add38b6a78d81fb23dda9f2c1a073521e6d82
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-10-24 00:23:10 -0300

    Update aplication controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    groups_path # Redirecciona a la página de grupos después de iniciar sesión
+  end
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
 end
