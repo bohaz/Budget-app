9eef2bf2e885d175b68d7b9436907296ec1e1a10
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-10-25 00:28:25 -0300

    Add methos

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -1,8 +1,34 @@
 class OperationsController < ApplicationController
+  before_action :authenticate_user!
   def index
     @group = Group.find(params[:group_id])
-    @operations = @group.operations
+    @operations = @group.operations.where(author: current_user)
+    @groups = Group.joins(:operations).where(operations: { author: current_user }).distinct
+  end  
+
+  def new
+    @group = Group.find(params[:group_id])
+    @operation = @group.operations.build
   end
 
+  def create
+    @group = Group.find(params[:group_id])
+    @operation = @group.operations.build(operation_params)
+    @operation.author = current_user
+  
+    if @operation.save
+      redirect_to group_operations_path(@group), notice: 'Operation was successfully created.'
+    else
+      render :new
+    end
+  end
+  
+
   def show; end
+
+  private
+
+  def operation_params
+    params.require(:operation).permit(:name, :amount)
+  end
 end
