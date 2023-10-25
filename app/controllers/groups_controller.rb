ab445ed49a7efa56278eeb6fbee57f9a90c79c91
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-10-25 00:27:59 -0300

    Add before action and update create method

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,7 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user!
   def index
-    @groups = Group.all
+    @groups = current_user.groups
   end
 
   def show; end
@@ -10,7 +11,7 @@ class GroupsController < ApplicationController
   end
 
   def create
-    @group = Group.new(group_params)
+    @group = current_user.groups.new(group_params)
     if @group.save
       redirect_to groups_path, notice: 'Group was successfully created.'
     else
