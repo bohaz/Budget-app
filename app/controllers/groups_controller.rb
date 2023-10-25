301d1fdf66d6ac980fe83b0757ee18277ef1c40d
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-10-25 16:03:15 -0300

    Add groups order

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,7 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!
   def index
-    @groups = current_user.groups
+    @groups = current_user.groups.order(created_at: :desc)
   end
 
   def show; end
